c6be13035bf10ad70651ac8f11cbb2cf95adb660
steve9948 <mbuguagichimu@gmail.com>
2023-10-23 11:58:12 -0700

    A function that prints the first x elements of a list and only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,23 @@
-#!/usr/bin/python3
-import sys
-
-def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+def safe_print_list_integers(my_list=None, x=0):
+    """
+    Print the first x elements of a list if they are integers.
+    
+    :param my_list: List of elements.
+    :param x: Number of elements to access in my_list.
+    :return: Number of integers printed.
+    """
+    count = 0  # Initialize a counter for the number of integers printed
     try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end=" ")
-                count += 1
-    except (IndexError, TypeError) as err:
-        print("Exception: {}".format(err), file=sys.stderr)
-        return count
-    else:
-        print()
-        return count
+        for i in range(x):  # Iterate through the range of x
+            try:
+                if isinstance(my_list[i], int):  # Check if the element at index i is an integer
+                    print("{:d}".format(my_list[i]), end=" ")  # Print the integer without a newline
+                    count += 1  # Increment the count for each integer printed
+            except IndexError:  # Handle the case where the index is out of range
+                break
+    except TypeError:  # Handle the case where my_list is not iterable
+        pass
+    finally:
+        print()  # Print a newline after printing the integers or encountering an error
+        return count  # Return the count of integers printed
 
