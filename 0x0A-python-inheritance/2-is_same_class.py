ad490558f8e950b84696260fbd3eaa4959d4bc9f
steve9948 <mbuguagichimu@gmail.com>
2023-11-06 09:26:03 -0800

    A functin that asserts if an object is an instance of the class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" 2-is_same_class: is_same_class """
+
+def is_same_class(obj, a_class):
+    '''Return true if obj is an instance of the the class else false.'''
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
