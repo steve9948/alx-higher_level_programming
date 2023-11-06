46824fbbd152db7e3bcf4a57b1830f2382c13c64
steve9948 <mbuguagichimu@gmail.com>
2023-11-06 09:28:15 -0800

    Removed the pycode style issue

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,7 +2,10 @@
 """ 2-is_same_class: is_same_class """
 
 def is_same_class(obj, a_class):
-    '''Return true if obj is an instance of the the class else false.'''
+    '''
+    Return true if obj is an instance 
+    of the the class else false
+    .'''
     if type(obj) == a_class:
         return True
     else:
