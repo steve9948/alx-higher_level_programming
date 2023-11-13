db5180f4dc2c2627752fbeb2447e9d2411d403ab
steve9948 <mbuguagichimu@gmail.com>
2023-11-13 21:09:39 +0000

    A pussh from intellijidea complete attempt

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python3
+""" Module: Rectagle inheriting from the base class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Rectangle: Instance object"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Instantiation of the instance"""
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+            getter for the function__width
+            Returns: The width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+            setter for thefunction width.
+            Args:
+                value (int): The value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+            getter for the height
+            Returns: height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            setter function for height
+            Args:
+                value (int): The value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+            getter function for x.
+            Returns: The value of x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+            setter function for x.
+            Args:
+                value (int): The value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+            getter function for y
+            Returns: The value of y
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+            setter function for y
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
+
+    def area(self):
+        """Calculates the area of a reactangle"""
+
+        return self.__width * self.__height
+
+    def display(self):
+        """ Print Rectangle instance
+        with the character #"""
+
+        for y in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """Returns the string representation of instance of a rectangle."""
+
+        s = "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
+        return s
+
+    def update(self, *args, **kwargs):
+        """ Allocates an argument to each attribute"""
+
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if type(value) != int and value is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                if key == "width":
+                    self.__width = value
+                if key == "height":
+                    self.__height = value
+                if key == "x":
+                    self.__x = value
+                if key == "y":
+                    self.__y = value
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of instance of a Rectangle."""
+
+        my_dict = {'id': self.id, 'width': self.__width,
+                   'height': self.__height, 'x': self.__x, 'y': self.__y}
+        return my_dict
