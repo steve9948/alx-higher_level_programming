bcff5e1444b846556e9a8544de1e58e5c045bcad
steve9948 <mbuguagichimu@gmil.com>
2024-01-09 19:32:58 -0800

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const Squared = require('./5-square.js');
+
+module.exports = class Square extends Squared {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+};
