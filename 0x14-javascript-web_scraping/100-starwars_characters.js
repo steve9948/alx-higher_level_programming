098e4b449031e766ada9cc621c1e78589c995c3b
steve9948 <mbuguagichimu@gmail.com>
2024-02-21 07:41:57 -0800

    Who was playing in this movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,10 +3,16 @@ const request = require('request');
 const id = process.argv[2];
 
 request(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true }, (err, res, body) => {
-  if (err) console.log(err);
+  if (err) {
+    console.error(err);
+    return;
+  }
   for (const character of body.characters) {
     request(character, { json: true }, (err, res, body) => {
-      if (err) console.log(err);
+      if (err) {
+        console.error(err);
+        return;
+      }
       console.log(body.name);
     });
   }
