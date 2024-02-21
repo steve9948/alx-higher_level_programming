7d789a9d7e00b3e5b72cd36cea29c409bb08b7bc
steve9948 <mbuguagichimu@gmail.com>
2024-02-21 02:54:28 -0800

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,9 @@
-const fs = require('fs')
-
-// Get file path from command line argument
-const filePath = process.argv[2]
-
-// Read the content of the file
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    // If an error occurred during reading, print the error object
-    console.error(err)
-  } else {
-    // If reading is successful, print the content of the file
-    console.log(data)
-  }
-})
-
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+try {
+  const data = fs.readFileSync(file, 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
